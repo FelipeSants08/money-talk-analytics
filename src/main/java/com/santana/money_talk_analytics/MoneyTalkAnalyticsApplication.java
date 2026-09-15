179a1fb8676337b107fddf5be8c19ad169a9630d
felipeSants08 <felipe_santana.01@hotmail.com>
2026-09-15 20:55:17 -0300

    Primeiro commit, projeto de análises de moedas

---
+++ src/main/java/com/santana/money_talk_analytics/MoneyTalkAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.santana.money_talk_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTalkAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyTalkAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santana/money_talk_analytics/MoneyTalkAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santana.money_talk_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTalkAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
